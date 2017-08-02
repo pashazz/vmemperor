836d3e0caf778718742cbd02e4c7559239250376
Andrey <a.tapekhin@ispras.ru>
2017-08-02 17:06:37 +0300

    really basic plan

--- vmemperor.py
+++ vmemperor.py
@@ -1,351 +1,133 @@
-from flask import Flask
-from flask import session as flask_session
-from flask import render_template, json, jsonify
-from flask import request
-
 from xenadapter import XenAdapter
-
-app = Flask(__name__, static_url_path='/static')
-
-global preseed
-preseed = dict()
-
-global status_dict
-status_dict = dict()
+import tornado.web
+import tornado.escape
+import json
+from abc import ABCMeta, abstractmethod
 
 
+class BaseHandler(tornado.web.RequestHandler):
+    def get_current_user(self):
+        return self.get_secure_cookie("user")
 
 
+class Authentication(metaclass=ABCMeta):
 
-@app.route("/auth", methods=['POST'])
-def authenticate():  # use outer authentication system tokens
-    """Sends a 401 response that enables basic auth"""
-    adapters = []
-    counter = 0
-    form = request.form if request.form else json.loads(request.data)
-    for endpoint in app.config['xen_endpoints']:
-        login = form['login' + str(counter)]
-        password = form['password' + str(counter)]
-        try:
-            adapters.append(XenAdapter(endpoint, flask_session, login, password))
-        except Exception as e:
-            flask_session.clear()
-            response = jsonify({'status': 'error', 'details': 'Auth failed',
-                                'reason': str(e)})
-            print(e)
-            response.status_code = 401
-            return response
+    @abstractmethod
+    def check_credentials(self, password, username):
+        """asserts credentials using inner db, or some outer authentication system"""
+        return
 
-    flask_session['is_su'] = all([endp["is_su"] for k, endp in flask_session.items()])
+    @abstractmethod
+    def get_user_groups(self, username):
+        """gets list of user groups"""
+        return
 
-    response = jsonify({'status': 'ok', 'details': 'Auth successful', 'reason': 'ok'})
-    response.status_code = 200
-    return response
+    @abstractmethod
+    def set_user_group(self, username, group):
+        """adds user to group"""
+        return
+
+    @abstractmethod
+    def set_user(self, username):
+        """creates cookie given username"""
+        return
 
 
-"""
-def admin_authenticate():
-    does what previous authenticate did
+@Authentication.register
+class DummyAuth(BaseHandler):
 
+    def check_credentials(self, password, username):
+        return True
 
-"""
+    def post(self):
+        username = self.get_argument("username", "")
+        password = self.get_argument("password", "")
+        authenticated = self.check_credentials(password, username)
+        if authenticated:
+            self.set_user(username)
+            self.write(json.dumps({}))
+        else:
+            return self.write(json.dumps({"error": "wrong credentials"}))
 
+    def set_user(self, username):
+        if username:
+            self.set_secure_cookie("user", tornado.escape.json_encode(username))
 
-"""
-def list_vms():
-    uses XenAdapter.get_vms_list()
-"""
-
-
-
-"""
-def list_templates():
-    uses XenAdapter.get_template_list()
-    sort by
-"""
-
-@app.route('/list-vms')
-def list_vms():
-    vm_list = []
-    for endpoint in app.config['xen_endpoints']:
-        adapter = XenAdapter(endpoint, flask_session)
-        vm_list.extend(adapter.get_vms_list())
-    return jsonify(vm_list)
-
-# should there be process, that checks system and updates info in database every n seconds(minutes) ?
-
-
-@app.route('/list-templates')
-def list_templates():  # requires to be logged in as an admin  (probably should cache this info into rethinkdb)
-    template_list = []
-    adapters = []
-    for endpoint in app.config['xen_endpoints']:
-        adapters.append(XenAdapter(endpoint, flask_session))
-
-    if 'is_su' in flask_session and flask_session['is_su']:
-        for adapter in adapters:
-            template_list.extend(adapter.get_template_list())
-        template_list = sorted(template_list, key=lambda k: (-len(k['tags']), k['name_label'].lower()))
-    else:
-        response = jsonify({'status': 'error', 'details': 'Failed to retrieve template list', 'reason': 'You have no pool administrator rights'})
-        response.status_code = 401
-        return response
-    return jsonify(template_list)
-
+        else:
+            self.clear_cookie("user")
 
 """
-def list_templates():
-    uses XenAdapter.retrieve_pool_info()
-    sort by
-"""
-
-@app.route('/list-pools', methods=['GET'])  # (probably should cache this info into rethinkdb)
-def list_pools():
-    pool_list = []
-    for endpoint in app.config['xen_endpoints']:
-        adapter = XenAdapter(endpoint, flask_session)
-        pool_info = adapter.retrieve_pool_info()
-        pool_info.update(endpoint)
-        pool_info['templates_enabled'] = [item for item in adapter.get_template_list() if 'tags' in item and 'vmemperor' in item['tags']]
-        pool_list.append(pool_info)
-
-    return jsonify(pool_list)
+views which get information should only get reverse polling with notifications from db (RethinkDB or CouchDB)
+information should be different for different users
+information in db should be updated in event loop
 
 
-def on_off_dispatcher(req, action):
-    form = req.form if req.form else json.loads(req.data)
-    vm_uuid = form.get('vm_uuid')
-    endpoint_url = form.get('endpoint_url')
-    if not vm_uuid or not endpoint_url:
-        response = {'status': 'error', 'details': 'Syntax error in your query', 'reason': 'missing argument'}
-        response.status_code = 406
-        return response
+list vms
+list vdi
+list networks
+create network?
+create vm
+start vm
+stop vm
+get vnc connection  -  http://xapi-project.github.io/xen-api/consoles.html
+attach vbd
+detach vbd
+destroy vm
+connect vm to network
 
-    endpoint = {'url': endpoint_url}
-    adapter = XenAdapter(endpoint, flask_session)
-    if action == "enable-template":
-        response_dict, response_status = adapter.capture_template(vm_uuid, True)
-    elif action == "disable-template":
-        response_dict, response_status = adapter.capture_template(vm_uuid, False)
-    elif action == "start-vm":
-        response_dict, response_status = adapter.start_stop_vm(vm_uuid, True)
-    elif action == "shutdown-vm":
-        response_dict, response_status = adapter.start_stop_vm(vm_uuid, False)
-    else:
-        response_dict = {'status': 'error', 'details': 'Something wrong in internals', 'reason': 'missing argument'}
-        response_status = 500
+list templates
+enable template
+capture template
 
-    response = jsonify(response_dict)
-    response.status_code = response_status
-    return response
-
-"""
-def create_vm():
-    uses xenadapter.create_vm
 """
 
 
-@app.route('/create-vm', methods=['POST'])
-def create_vm():
-    form = request.form if request.form else json.loads(request.data)
-    print(form)
-    """
-    template-select -> template_uuid
-    storage-select -> sr
-    network-select  -> network_uuid
-    add os_kind argument
-    """
-
-    if not form.get("template-select") or \
-        not form.get("pool-select") or \
-        form.get("template-select") == "--" or \
-        form.get("pool-select") == "--" or \
-        not form.get("username") or \
-        not form.get("hostname") or \
-        not form.get("password"):
-        response = jsonify({'status': 'error', 'details': 'You have missing obligatory argument', 'reason': 'missing argument'})
-        response.status_code = 400
-        return response
-    if form.get("password") != form.get("password2"):  # check by token prior to view
-        response = jsonify({'status': 'error', 'details': 'Your passwords do not match', 'reason': 'wrong argument'})
-        response.status_code = 400
-        return response
-
-    endpoint = {'url': form["pool-select"]}
-    adapter = XenAdapter(endpoint, flask_session)
-
-    vm, mirror_url, mirror_path = adapter.create_vm(form["template-select"], form["hostname"], form["vcpus"], int(form["ram"])*1024*1024, form["hdd"], form["storage-select"], form["network-select"], "ubuntu", app.config['vmemperor-address'] + "preseed")
-
-    preseed[vm] = form
-    preseed[vm]["mirror_url"] = mirror_url
-    preseed[vm]["mirror_path"] = mirror_path
-
-    response = jsonify({"uuid": vm})
-    status_dict[vm] = ("System installation initiated", 20)
-    response.status_code = 200
-    return response
+class AnsibleHooks:
+    pass
 
 
-"""
-def get_preseed():
-    todo
-"""
-
-@app.route('/preseed/<oskind>/<id>')
-def get_preseed(oskind, id):
-    return render_template("installation-scenarios/ubuntu.jinja2", opts=preseed[id])
-
-"""
-def get_status():
-    use status in rethinkdb
-    todo
-"""
+def event_loop():
+    pass
 
 
-"""
-def statistic loop():
-    periodically asks everything about their status and updates rethinkdb
-"""
+def make_app(auth_class = DummyAuth):
+    return tornado.web.Application([
+        (r"/login", auth_class),
+    ])
 
 
-@app.route('/status-vm', methods=['POST'])
-def get_status():  # use rethinkdb
-    form = request.form if request.form else json.loads(request.data)
-    status_d = {}
-    for vm in form['ids']:
-        if status_dict.get(vm):
-            status_d[vm] = {"name": vm, "status": status_dict[vm][0], "details": status_dict[vm][1]}
+def main():
+    """ reads config in ini and configures system"""
 
-    return jsonify(status_d)
+    return
 
-"""
-def start_vm():
-    adapter.start_stop_vm
-"""
+if __name__ == '__main__':
+    main()
 
-@app.route('/start-vm', methods=['POST'])  # rename to start_vm
-def start_vm():
-    return on_off_dispatcher(request, "start-vm")
-
-"""
-def shutdown_vm():
-    adapter.start_stop_vm
-"""
-
-@app.route('/shutdown-vm', methods=['POST'])  # rename to shutdown_vm
-def shutdown_vm():
-    return on_off_dispatcher(request, "shutdown-vm")
+# @app.route('/')
+# def secret_page():
+#     return render_template('index.html')
+#
+#
+# @app.route('/pool-index', methods=["GET"])
+# def pool_index():
+#     return jsonify(app.config['xen_endpoints'])
 
 
-"""
-def enable_template():
-    adapter.capture_template
-"""
-@app.route('/enable-template', methods=['POST'])  # rename to enable_template
-def enable_template():
-    return on_off_dispatcher(request, "enable-template")
-
-
-"""
-def disable_template():
-    adapter.capture_template
-"""
-@app.route('/disable-template', methods=['POST'])  # rename to disable_template
-def disable_template():
-    return on_off_dispatcher(request, "disable-template")
-
-
-"""
-def disable_template():
-    adapter.set_install_options
-"""
-
-@app.route('/update-template', methods=['POST'])  # rename to update_template
-def update_template():
-    form = request.form if request.form else json.loads(request.data)
-    vm_uuid = form.get('vm_uuid')
-    endpoint_url = form.get('endpoint_url')
-    mirror = form.get('default_mirror')
-    hooks_dict = form.get('vmemperor_hooks')
-    endpoint = {'url': endpoint_url}
-    adapter = XenAdapter(endpoint, flask_session)
-    response_dict, response_status = adapter.set_install_options(vm_uuid, hooks_dict, mirror)
-    response = jsonify(response_dict)
-    response.status_code = response_status
-    return response
-
-
-@app.route('/logout', methods=['POST', 'GET'])  # removes token from cookies
-def logout():
-    flask_session.clear()
-    response = jsonify({'status': 'success', 'details': 'Logout successful', 'reason': ''})
-    response.status_code = 200
-    return response
-
-"""
-def list_vdi()
-    lists vdi
-"""
-
-
-"""
-def create_vbd():
-    gets vm uuid and vbi uuid and creates vbd
-
-"""
-
-"""
-def plug_vbd():
-    gets vbd uuid and plugs it in associated vm
-
-"""
-
-"""
-def add_hook():
-    gets ansible file and variables template from user and stores it
-    think harder
-"""
-
-
-"""
-def use_hook():
-    gets information about which vm to run which ansible playbook onto
-    gets information for variables template and launches ansible playbook
-    think harder
-"""
-
-
-"""
-def get_vnc_connection():
-    http://xapi-project.github.io/xen-api/consoles.html
-    think harder
-"""
-
-
-@app.route('/')
-def secret_page():
-    return render_template('index.html')
-
-
-@app.route('/pool-index', methods=["GET"])
-def pool_index():
-    return jsonify(app.config['xen_endpoints'])
-
-
-#app.secret_key = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(32)])
-app.secret_key = 'SADFccadaeqw221fdssdvxccvsdf'
-if __name__ == '__main__':  # everything should be loaded from ini
-    #app.config.update(SESSION_COOKIE_SECURE=True)
-    app.config['SESSION_COOKIE_HTTPONLY'] = False
-    app.config['xen_endpoints'] = [
-        {'id': 'http://10.10.10.18:80/', 'url': 'http://10.10.10.18:80/', 'description': 'Pool A'},
-        #{'id': 'http://10.10.10.18:80//', 'url': 'http://10.10.10.18:80/', 'description': 'Pool Z'},
-        #{'id': 'http://172.31.0.32:80/', 'url': 'http://172.31.0.32:80/', 'description': 'Pool Z'}
-        ]
-    app.config['supported-distros'] = {'debianlike': 'all'}
-    app.config['enabled-distros'] = app.config['supported-distros']
-    app.config['supported-reverse-proxies'] = {'vmemperor-nginx': 'Nginx configuration files'}
-    app.config['enabled-reverse-proxies'] = app.config['supported-reverse-proxies']
-    app.config['vmemperor-address'] = 'http://localhost:5000/'
-    #retrieve_vms_list(session)
-    app.run(debug=True, use_reloader=True, threaded=False, host="0.0.0.0", port=5000)
+# #app.secret_key = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(32)])
+# app.secret_key = 'SADFccadaeqw221fdssdvxccvsdf'
+# if __name__ == '__main__':  # everything should be loaded from ini
+#     #app.config.update(SESSION_COOKIE_SECURE=True)
+#     app.config['SESSION_COOKIE_HTTPONLY'] = False
+#     app.config['xen_endpoints'] = [
+#         {'id': 'http://10.10.10.18:80/', 'url': 'http://10.10.10.18:80/', 'description': 'Pool A'},
+#         #{'id': 'http://10.10.10.18:80//', 'url': 'http://10.10.10.18:80/', 'description': 'Pool Z'},
+#         #{'id': 'http://172.31.0.32:80/', 'url': 'http://172.31.0.32:80/', 'description': 'Pool Z'}
+#         ]
+#     app.config['supported-distros'] = {'debianlike': 'all'}
+#     app.config['enabled-distros'] = app.config['supported-distros']
+#     app.config['supported-reverse-proxies'] = {'vmemperor-nginx': 'Nginx configuration files'}
+#     app.config['enabled-reverse-proxies'] = app.config['supported-reverse-proxies']
+#     app.config['vmemperor-address'] = 'http://localhost:5000/'
+#     #retrieve_vms_list(session)
+#     app.run(debug=True, use_reloader=True, threaded=False, host="0.0.0.0", port=5000)
--- xenadapter.py
+++ xenadapter.py
@@ -5,231 +5,28 @@ import provision
 
 
 class XenAdapter:
-    def __init__(self, endpoint, flask_session, login=None, password=None):
-        """
-        @type session: XenAPI.Session
-        """
-        self.endpoint = endpoint
 
-        url = endpoint['url']
-        session = XenAPI.Session(url)
-        if not login or not password:
-            if flask_session and \
-                            url in flask_session and \
-                            'login' in flask_session[url] and 'password' in flask_session[url]:
-                login = flask_session[url]['login']
-                password = flask_session[url]['password']
-            else:
-                raise Exception("Unauthorized")
-        else:
-            flask_session[url] = {'url': endpoint['url'], 'login': login, 'password': password}
-        session.login_with_password(login, password)
-        if session.status() == "Failure":
-            raise Exception
-        flask_session[url]['is_su'] = session.xenapi.session.get_is_local_superuser(session._session)
-        self.session = session
-        self.api = self.session.xenapi
+    def __init__(self):
+        """creates session connection to XenAPI"""
+        return
+"""
+list vms
+list vdi
+list networks
+create network?
+create vm
+start vm
+stop vm
+get vnc connection
+create vbd
+attach vbd
+detach vbd
+destroy vbd
+destroy vm
+connect vm to network
 
-    def get_template_list(self):
-        print("Getting template list using fast method")
-        template_list = []
-        all_records = self.api.VM.get_all_records()
-        for record in all_records.values():
-            entry = dict()
-            for field in ['is_control_domain', 'is_a_template', 'is_a_snapshot']:
-                entry[field] = record[field] if field in record and record[field] != 'OpaqueRef:NULL' else None
-            if not entry['is_control_domain'] and entry['is_a_template'] and not entry['is_a_snapshot']:
-                for field in ['uuid', 'name_label', 'name_description', 'allowed_operations', 'tags', 'other_config']:
-                    entry[field] = record[field] if field in record and record[field] != 'OpaqueRef:NULL' else None
-                entry['other_config']['vmemperor_hooks'] = hooks.merge_with_dict(entry['other_config'].get('vmemperor_hooks'))
-                entry['other_config']['vmemperor_hooks_meta'] = hooks.get_hooks_with_manifest(entry['other_config']['vmemperor_hooks'])
-                entry['endpoint'] = self.endpoint
-                if 'ubuntu' in record['name_label'].lower():
-                    entry['default_mirror'] = 'http://mirror.yandex.ru/ubuntu/'
-                elif 'debian' in record['name_label'].lower():
-                    entry['default_mirror'] = 'http://mirror.yandex.ru/debian/'
-                else:
-                    entry['default_mirror'] = ''
+list templates
+enable template
+capture template
 
-                template_list.append(entry)
-        return template_list
-
-    def retrieve_pool_info(self):
-        pool_info = dict()
-
-        networks = self.api.network.get_all_records()
-        # TODO: implement filtering by tags some time
-        pool_info["networks"] = []
-        for net in networks.values():
-            if net["name_label"] == "Host internal management network":
-                continue
-            pool_info["networks"].append({"uuid": net["uuid"], "name_label": net["name_label"]})
-
-        pool_info["storage_resources"] = []
-        storage_resources = self.api.SR.get_all_records()
-        for sr in storage_resources.values():
-            if sr["type"] == "lvmoiscsi" or sr["type"] == "lvm":
-                label = ''.join((sr["name_label"], " (Available %d GB)" % ((int(sr['physical_size']) - int(sr['virtual_allocation']))/(1024*1024*1024))))
-                if sr["shared"]:
-                    label = ''.join(("Shared storage: ", label))
-                    pool_info["storage_resources"].insert(0, {"uuid": sr["uuid"], "name_label": label})
-                else:
-                    pool_info["storage_resources"].append({"uuid": sr["uuid"], "name_label": label})
-
-        pools = self.api.pool.get_all_records()
-        default_sr = None
-        for pool in pools.values():
-            default_sr = pool['default_SR']
-        if default_sr != 'OpaqueRef:NULL':
-            sr_info = self.api.SR.get_record(default_sr)
-            pool_info['hdd_available'] = (int(sr_info['physical_size']) - int(sr_info['virtual_allocation']))/(1024*1024*1024)
-        else:
-            pool_info['hdd_available'] = None
-        pool_info['host_list'] = []
-
-        records = self.api.host.get_all_records()
-        for host_ref, record in records.items():
-            metrics = self.api.host_metrics.get_record(record['metrics'])
-            host_entry = dict()
-            for i in ['name_label', 'resident_VMs', 'software_version', 'cpu_info']:
-                host_entry[i] = record[i]
-            host_entry['memory_total'] = int(metrics['memory_total'])/(1024*1024)
-            host_entry['memory_free'] = int(metrics['memory_free'])/(1024*1024)
-            host_entry['live'] = metrics['live']
-            host_entry['memory_available'] = int(self.api.host.compute_free_memory(host_ref))/(1024*1024)
-            pool_info['host_list'].append(host_entry)
-        print(pool_info)
-        return pool_info
-
-    def get_vms_list(self):
-        vm_list = []
-        all_records = self.api.VM.get_all_records()
-        for record in all_records.values():
-            entry = dict()
-            for field in ['is_control_domain', 'is_a_template', 'is_a_snapshot']:
-                entry[field] = record[field] if field in record and record[field] != 'OpaqueRef:NULL' else None
-            if not max([x for x in entry.values()]):
-                for field in ['uuid', 'name_label', 'name_description', 'allowed_operations',
-                              'power_state', 'VCPUs_at_startup', 'memory_target', 'guest_metrics',
-                              'memory_dynamic_min', 'memory_dynamic_max']:
-                    entry[field] = record[field] if field in record and record[field] != 'OpaqueRef:NULL' else None
-                entry['networks'] = self.api.VM_guest_metrics.get_record(entry['guest_metrics'])['networks'] if entry['guest_metrics'] else None
-                entry['endpoint'] = self.endpoint
-
-                vm_list.append(entry)
-
-        return vm_list
-
-    def capture_template(self, vm_uuid, enable):
-        vm_ref = self.api.VM.get_by_uuid(vm_uuid)
-        try:
-            if enable:
-                self.api.VM.add_tags(vm_ref, 'vmemperor')
-            else:
-                self.api.VM.remove_tags(vm_ref, 'vmemperor')
-            return {'status': 'success', 'details': 'template modified', 'reason': ''}, 200
-        except XenAPI.Failure as e:
-            return {'status': 'error', 'details': 'can not modify template', 'reason': e.details}, 409
-        except Exception as e:
-            return {'status': 'error', 'details': 'can not modify template', 'reason': str(e)}, 500
-
-    def start_stop_vm(self, vm_uuid, enable):
-        vm_ref = self.api.VM.get_by_uuid(vm_uuid)
-        try:
-            if enable:
-                self.api.VM.start(vm_ref, False, False)
-            else:
-                self.api.VM.shutdown(vm_ref)
-            return {'status': 'success', 'details': 'VM power state changed', 'reason': ''}, 200
-        except XenAPI.Failure as e:
-            return {'status': 'error', 'details': 'Can not change VM power state', 'reason': e.details}, 409
-        except Exception as e:
-            return {'status': 'error', 'details': 'Can not change VM power state', 'reason': str(e)}, 500
-
-    def set_install_options(self, vm_uuid, hooks_dict, mirror):
-        vm_ref = self.api.VM.get_by_uuid(vm_uuid)
-        internal_hooks = hooks.generate_other_config_entry(hooks_dict)
-        try:
-            self.api.VM.remove_from_other_config(vm_ref, 'vmemperor_hooks')
-            self.api.VM.remove_from_other_config(vm_ref, 'default_mirror')
-            self.api.VM.remove_from_other_config(vm_ref, 'install-repository')
-        except:
-            pass
-        try:
-            self.api.VM.add_to_other_config(vm_ref, 'vmemperor_hooks', internal_hooks)
-            self.api.VM.add_to_other_config(vm_ref, 'default_mirror', mirror)
-            self.api.VM.add_to_other_config(vm_ref, 'install-repository', mirror)
-            return {'status': 'success', 'details': 'Install options updated', 'reason': ''}, 200
-        except XenAPI.Failure as e:
-            return {'status': 'error', 'details': 'Can not set install options', 'reason': e.details}, 409
-        except Exception as e:
-            return {'status': 'error', 'details': 'Can not set install options', 'reason': str(e)}, 500
-
-    def create_vm(self, template_uuid, hostname, vcpus, ram, hdd, sr, network_uuid, os_kind, preseed_prefix):
-        api = self.api
-        template = api.VM.get_by_uuid(template_uuid)
-        tmpl = api.VM.get_record(template)
-        vm_ref = api.VM.clone(template, hostname)
-        vm = api.VM.get_record(vm_ref)
-
-        nets = api.network.get_all_records()
-        net = api.network.get_by_uuid(network_uuid)
-        network = api.network.get_record(net)
-        print(network["bridge"])
-        vif = api.VIF.create({'network': net, 'VM': vm_ref, 'device': "0", "MAC": "", "MTU": network["MTU"], "other_config": {}, "qos_algorithm_type": "",
-                              "qos_algorithm_params": {}})
-        preseed = "".join((preseed_prefix, "/", os_kind, "/", vm["uuid"]))
-        if os_kind == "ubuntu":
-            pv_args = "netcfg/get_hostname=%s console=hvc0 debian-installer/locale=en_US console-setup/layoutcode=us console-setup/ask_detect=false interface=eth0 netcfg/disable_dhcp=false preseed/url=%s" % (hostname, preseed)
-        else:
-            pv_args = ""
-
-        api.VM.set_PV_args(vm_ref, pv_args)
-        spec = provision.getProvisionSpec(self.session, vm_ref)
-        spec.setSR(sr)
-        provision.setProvisionSpec(self.session, vm_ref, spec)
-        vm_platform = api.VM.get_platform(vm_ref)
-        vm_platform["cores-per-socket"] = vcpus
-        api.VM.set_platform(vm_ref, vm_platform)
-        api.VM.set_VCPUs_max(vm_ref, vcpus)
-        api.VM.set_VCPUs_at_startup(vm_ref, vcpus)
-        api.VM.set_memory_limits(vm_ref, str(ram), str(ram), str(ram), str(ram))
-        api.VM.provision(vm_ref)
-        api.VM.start(vm_ref, False, True)
-
-        repo = vm["other_config"]["install-repository"].replace("http://", "")
-        repo = repo.replace("ftp://", "")
-
-        mirror_url, mirror_path = repo.split("/", 1)
-        mirror_path = "".join(("/", mirror_path))
-        return vm["uuid"], mirror_url, mirror_path
-
-    """
-    def destroy_vm(self, vm_uuid, enable):
-        should be in halt state prior to destroying
-        return {'status': string 'details': string, 'reason': string}
-    """
-
-    """
-    def list_vdi(self):
-        get_all
-        return {'status': string 'details': string, 'reason': string}
-    """
-
-    """
-    def create_vbd(self, vdi_uuid, vm_uuid):
-
-        return {'status': string 'details': string, 'reason': string}
-    """
-
-
-    """
-    def plug_vbd(self,  vbd_uuid):
-
-        return {'status': string 'details': string, 'reason': string}
-    """
-
-    """
-    def get_vnc_connection():
-        http://xapi-project.github.io/xen-api/consoles.html
-    """
+"""
