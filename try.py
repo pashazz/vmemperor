2bfdeab4ffb8bda5ad8530dd96ee04905dc72056
bura2017 <valeriya.shvetcova@phystech.edu>
2017-08-10 19:09:10 +0300

    nu nado

--- try.py
+++ try.py
@@ -1,5 +1,4 @@
 from configparser import ConfigParser
-from XenAPI import Failure
 from xenadapter import XenAdapter
 
 def print_list (list):
@@ -47,11 +46,28 @@ def main():
         sr_uuid = choose_sr(xen.api.SR.get_all_records())
         tmpl_uuid = choose_tmpl(xen.list_templates())
         net_uuid = choose_net(xen.api.network.get_all_records())
-        xen.create_vm(tmpl_uuid, sr_uuid, net_uuid, 0, 'try_ku')
+        vdi_size = 57767936
+        xen.create_vm(tmpl_uuid, sr_uuid, net_uuid, str(vdi_size), 'try_ku')
         vms = xen.list_vms()
         print(len(vms))
 
+        # xen.create_disk(sr_uuid, vdi_size)
 
+        # vdi_uuid = 'd77ae934-7a16-4016-99e7-7f2f11de170d'
+        # vm_uuid = '3baf280c-702f-f40c-77f6-b6e7e5ea8bbb'
+        # vdi_ref = xen.api.VDI.get_by_uuid(vdi_uuid)
+        # vdi = xen.api.VDI.get_record(vdi_ref)
+        # vbds = vdi['VBDs']
+        # vbd_ref = vbds[0]
+        # vbd_uuid = xen.api.VBD.get_uuid(vbd_ref)
+        # xen.detach_disk(vbd_uuid)
+
+        # vbd_uuid = xen.attach_disk(vm_uuid, vdi_uuid)
+        # print(vbd_uuid)
+        # vbd_ref = xen.api.VBD.get_by_uuid(vbd_uuid)
+        # vbd = xen.api.VBD.get_record(vbd_ref)
+        # for key, val in vbd.items():
+        #     print(key, val)
 
     finally:
         xen.session._logout()
