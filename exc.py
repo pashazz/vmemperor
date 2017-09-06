b4613676f850fdfc13ff2d05944914ad34bec98d
Pavel Zinin <pzinin@gmail.com>
2017-09-06 21:22:33 +0300

    added functions to LDAP auth class: get_all_groups, get_group_name_by_id (class methods), get_user_groups returns dict : id -> name

--- exc.py
+++ exc.py
@@ -1,6 +1,6 @@
 class EmperorException(Exception):
-    def __init__(self, exc_obj, message):
-        exc_obj.log.error("{0}: {1}".format(type(self).__name__, message))
+    def __init__(self, log, message):
+        log.error("{0}: {1}".format(type(self).__name__, message))
         super().__init__()
         self.message = message
 
@@ -28,19 +28,19 @@ class AuthenticationRealmException(AuthenticationException):
     pass
 
 class AuthenticationUserNotFoundException(AuthenticationException):
-    def __init__(self, exc_obj):
-        super().__init__(exc_obj, "Realm {0} can't find user {1}".format(type(exc_obj).__name__, exc_obj.username))
+    def __init__(self, log, realm):
+        super().__init__(log, "Realm {0} can't find user {1}".format(type(realm).__name__, realm.username))
 
 
 class AuthenticationPasswordException(AuthenticationException):
-    def __init__(self, exc_obj):
-        super().__init__(exc_obj, "Realm {0} can't find authenticate user {1}: incorrect password".format(type(exc_obj).__name__, exc_obj.username))
+    def __init__(self, log, realm):
+        super().__init__(log, "Realm {0} can't find authenticate user {1}: incorrect password".format(type(realm).__name__, realm.username))
 
 class AuthenticationWithEmptyPasswordException(AuthenticationException):
-    def __init__(self, exc_obj):
-        super().__init__(exc_obj,
-                         "Realm {0} can't find authenticate user {1}: empty password".format(type(exc_obj).__name__,
-                                                                                                 exc_obj.username))
+    def __init__(self, log, realm):
+        super().__init__(log,
+                         "Realm {0} can't find authenticate user {1}: empty password".format(type(realm).__name__,
+                                                                                                 realm.username))
 
 class UnauthorizedException(AuthenticationException):
     pass
