885e9e4a272133bb67668db5d7255e580d401e00
Pavel Zinin <pzinin@gmail.com>
2019-02-13 17:50:17 +0300

    Fix Option type handling in Select component

--- frontend/app/components/Select/index.tsx
+++ frontend/app/components/Select/index.tsx
@@ -1,37 +1,37 @@
-import { FieldProps } from 'formik'
+import {FieldProps} from 'formik'
 import React from 'react'
-import Select, { Option, ReactSelectProps } from 'react-select'
+import Select, {Option, ReactSelectProps} from 'react-select'
 import {FormFeedback, InputGroup, InputGroupAddon} from "reactstrap";
 import FormGroup from "reactstrap/lib/FormGroup";
 
 
-
 /* See bug https://github.com/JedWatson/react-select/issues/1453
  and https://github.com/JedWatson/react-select/issues/2930 */
-const SelectField: React.FunctionComponent<ReactSelectProps & FieldProps> = ({
-  options,
-  field,
-  form,
-  placeholder
-}) => (
+const SelectField: React.FunctionComponent<ReactSelectProps & FieldProps>
+  = ({
+       options,
+       field,
+       form,
+       placeholder
+     }) => (
   <FormGroup style={{paddingRight: "20px", paddingLeft: "20px"}}>
-  <div style={{ margin: '1rem 0' }}>
-  <Select
-    options={options}
-    name={field.name}
-    value={options ? options.find(option => option.value === field.value) : ''}
-    onChange={(option: Option) => form.setFieldValue(field.name, option)}
-    onBlur={field.onBlur}
-    placeholder={placeholder}
-  />
+    <div style={{margin: '1rem 0'}}>
+      <Select
+        options={options}
+        name={field.name}
+        value={options ? options.find(option => option === field.value) : ''}
+        onChange={(option: Option) => form.setFieldValue(field.name, option)}
+        onBlur={field.onBlur}
+        placeholder={placeholder}
+      />
 
-    {!!form.errors[field.name] && form.touched[field.name] && (
-      <FormFeedback>
-        {form.errors[field.name]}
-      </FormFeedback>
-    )
-    }
-  </div>
+      {!!form.errors[field.name] && form.touched[field.name] && (
+        <FormFeedback>
+          {form.errors[field.name]}
+        </FormFeedback>
+      )
+      }
+    </div>
   </FormGroup>
 );
 
